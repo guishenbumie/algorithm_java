593a6379401af58c1dfba8e0429de049b86476fa
sunfengmao <sunfengmao@bytedance.com>
2026-01-21 15:41:08 +0800

    init

---
+++ algorithm/src/main/java/a_sort/QuickSort.java
@@ -0,0 +1,11 @@
+package a_sort;
+
+public class QuickSort {
+    public static void main(String[] args) {
+
+    }
+
+    public static void quickSort(int[] arr, int left, int right) {
+
+    }
+}
---
+++ algorithm/src/main/java/a_sort/SelectSort.java
@@ -0,0 +1,11 @@
+package a_sort;
+
+public class SelectSort {
+    public static void main(String[] args) {
+
+    }
+
+    public static void selectSort(int[] arr) {
+
+    }
+}
